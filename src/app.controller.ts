5c89ffcf923eafef00cf1194360f9971f6e6cd25
Fabio Neves <fabinhoneves09@gmail.com>
2023-04-07 18:59:38 -0300

    fix: removed auth guard from health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { AuthGuard } from './common/decorators/guards/auth.decorator';
 
 @Controller('/health')
 @ApiTags('Health Check')
 export class AppController {
+
   @Get()
-  @UseGuards(new AuthGuard('DEV'))
   healthCheck(): string {
     return 'Ok';
   }
