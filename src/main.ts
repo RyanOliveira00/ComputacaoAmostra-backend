649ddf7a3afb2a8150112648550c9ab4451cbd38
Fabio Neves <fabinhoneves09@gmail.com>
2023-05-08 15:45:05 -0300

    docs: updated docs

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { configurationService } from './config/config.service';
 import { resolve } from 'path';
-import { writeFileSync } from 'fs';
+import { createWriteStream, writeFileSync } from 'fs';
+import { get } from 'http';
 
 const PORT = configurationService.getValue('PORT');
 
@@ -33,6 +34,47 @@ async function bootstrap() {
     const swaggerJson = JSON.stringify(document, null, 2);
     writeFileSync(pathToSwaggerJson, swaggerJson);
     console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
+    if (process.env.NODE_ENV === 'development') {
+    // write swagger ui files
+    get(
+      `http://localhost:${PORT}/swagger/swagger-ui-bundle.js`,
+      function (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+        console.log(
+          `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`
+        );
+      }
+    );
+
+    get(
+      `http://localhost:${PORT}/swagger/swagger-ui-init.js`,
+      function (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+        console.log(
+          `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`
+        );
+      }
+    );
+
+    get(
+      `http://localhost:${PORT}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js')
+        );
+        console.log(
+          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`
+        );
+      }
+    );
+
+    get(`http://localhost:${PORT}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+      console.log(
+        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`
+      );
+    });
+  }  
   }
   app.listen(PORT, () => `Listening to port: ${PORT}`);
 }
